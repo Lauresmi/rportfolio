ed35b87b8daf2fef984a1ea29b76fdd2cd45e1ac
Lauresmi <84407932+Lauresmi@users.noreply.github.com>
2022-03-29 20:25:23 +0300

    Initial commit

---
+++ src/Components/Header/CallToAction.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CallToAction = () => {
+  return (
+    <div className='cta'>
+        <a href="">Download CV</a>
+        <a href="#contact">Lets connect</a>
+
+    </div>
+  )
+}
+
+export default CallToAction
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I`m</h5>
+        <h1>Laura Esmite</h1>
+        <h5 className='text-light'>Junior Front-end Developer</h5>
+        
+      </div>
+    </header>
+  )
+}
+
+export default Header
